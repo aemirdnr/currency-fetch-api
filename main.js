0f16217ae691c1970f49378be997562d1df22ad1
aemirdnr <aemirdnr@gmail.com>
2022-07-13 00:38:23 +0300

    Fix & Refactor

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 let selectCurrency = document.getElementById("currency-select")
 let currencyContainer = document.getElementById("currency-container")
 
-let currencySymbols = []
-let currencyPrice = []
-
 function getData() {
     let baseCurrency = selectCurrency.value
+
     currencyContainer.innerHTML = ""
 
     let api = "https://api.exchangerate.host/latest?base=" + baseCurrency
@@ -13,19 +11,16 @@ function getData() {
     fetch(api)
     .then(response => response.json())
     .then(currency => {
-        currencySymbols.push(Object.keys(currency.rates))
-        currencyPrice.push(Object.values(currency.rates))
-    
-        for (let i = 0; i < currencySymbols[0].length; i++) {
-            let symbol = currencySymbols[0][i]
-            let price = currencyPrice[0][i]
-    
+        Object.entries(currency.rates).forEach(item => {
+            const symbol = item[0]
+            const price = item[1]
+
             currencyContainer.innerHTML += `
-                <div class="col-12 col-md-5 col-lg-3 bg-dark rounded-3 p-2">
-                    ${symbol}/${baseCurrency}<br>1 ${baseCurrency} = ${price} ${symbol}
-                </div>
+            <div class="col-12 col-md-5 col-lg-3 bg-dark rounded-3 p-2">
+                ${symbol}/${baseCurrency}<br>1 ${baseCurrency} = ${price} ${symbol}
+            </div>
             `
-        }
+        })
     })
 }
 
